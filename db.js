9d410133c5de6e54965592105f4afb4587e6740b
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2025-05-10 17:07:46 +0330

    add saveTask method for edit and add task to DB class

--- db.js
+++ db.js
@@ -41,8 +41,12 @@ export default class DB {
     if (DB.DBExists()) {
       data = fs.readFileSync(filename, "utf-8");
     } else {
-      DB.createDB();
-      return false;
+      try {
+        DB.createDB();
+        return false;
+      } catch (error) {
+        throw new Error(error.message);
+      }
     }
     try {
       data = JSON.parse(data);
@@ -57,8 +61,12 @@ export default class DB {
     if (DB.DBExists()) {
       data = fs.readFileSync(filename, "utf-8");
     } else {
-      DB.createDB();
-      return false;
+      try {
+        DB.createDB();
+        return false;
+      } catch (error) {
+        throw new Error(error.message);
+      }
     }
     try {
       data = JSON.parse(data);
@@ -74,14 +82,85 @@ export default class DB {
     if (DB.DBExists()) {
       data = fs.readFileSync(filename, "utf-8");
     } else {
-      DB.createDB();
-      return [];
+      try {
+        DB.createDB();
+        return [];
+      } catch (error) {
+        throw new Error(error.message);
+      }
     }
     try {
       data = JSON.parse(data);
       return data;
     } catch (error) {
       throw new Error("Syntax error \n Please check the DB file.");
+    }
+  }
+  static saveTask(title, completed = false, id = 0) {
+    id = Number(id);
+    if (id < 0 || id !== parseInt(id)) {
+      throw new Error("id must be an integer, equal or greator than zero.");
+    } else if (typeof title !== "string" || title.length < 3) {
+      throw new Error("title must contain at least 3 letters.");
+    }
+
+    const task = DB.getTaskByTitle(title);
+    if (task && task.id !== id) {
+      throw new Error("A task exists with this title.");
+    }
+
+    let data;
+    if (DB.DBExists()) {
+      data = fs.readFileSync(filename, "utf-8");
+    } else {
+      try {
+        DB.createDB();
+        data = "[]";
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    }
+
+    try {
+      data = JSON.parse(data);
+    } catch (error) {
+      throw new Error("Syntax error \n Please check the DB file.");
+    }
+
+    if (id === 0) {
+      if (data.length === 0) {
+        id = 1;
+      } else {
+        id = data[data.length - 1].id + 1;
+      }
+      data.push({
+        id,
+        title,
+        completed,
+      });
+      const str = JSON.stringify(data, null, "    ");
+      try {
+        fs.writeFileSync(filename, str, "utf-8");
+        return true;
+      } catch (error) {
+        throw new Error("Can not save the task.");
+      }
+    } else {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].id === id) {
+          data[i].title = title;
+          data[i].completed = completed;
+
+          const str = JSON.stringify(data, null, "    ");
+          try {
+            fs.writeFileSync(filename, str, "utf-8");
+            return true;
+          } catch (error) {
+            throw new Error("Can not save the task.");
+          }
+        }
+      }
+      throw new Error("Task not fouond.");
     }
   }
 }
