a0305fc1e0dde040b59d3244505da136c2ab8eae
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2025-05-12 09:22:03 +0330

    add getTaskById to Task class

---
+++ task.js
@@ -0,0 +1,65 @@
+import util from "util";
+
+import chalk from "chalk";
+
+import DB from "./db.js";
+
+export default class Task {
+  #id = 0;
+  #title;
+  #completed;
+
+  constructor(title, completed = false) {
+    this.title = title;
+    this.completed = completed;
+  }
+
+  get id() {
+    return this.#id;
+  }
+  get title() {
+    return this.#title;
+  }
+  set title(value) {
+    if (typeof value !== "string" || value.length < 3) {
+      throw new Error("title must contain at least 3 letters.");
+    }
+    this.#title = value;
+  }
+  get completed() {
+    return this.#completed;
+  }
+
+  set completed(value) {
+    this.#completed = Boolean(value);
+  }
+
+  [util.inspect.custom]() {
+    return `Task {
+    id:${chalk.bgYellowBright(this.id)}
+    title${chalk.green('"' + this.title + '"')}
+    completed:${chalk.blueBright(this.completed)}
+    }`;
+  }
+  save() {
+    try {
+      const id = DB.saveTask(this.#title, this.#completed, this.#id);
+      this.#id = id;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  static getTaskById(id) {
+    const task = DB.getTaskById(id);
+    if (task) {
+      const item = new Task(task.title, task.completed);
+      item.#id = id;
+      return item;
+    } else {
+      return false;
+    }
+  }
+  static getTaskByTitle(title) {}
+  static getAllTasks() {}
+}
