627069cbcb5333d9bc9a7126317627565a8a90e2
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2025-05-13 15:12:55 +0330

    Add basic task listing functionality with console table output

---
+++ action.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+import axios from "axios";
+import { parse, stringify } from "csv/sync";
+
+import DB from "./db.js";
+import Task from "./Task.js";
+
+const error = chalk.redBright.bold;
+const warn = chalk.yellowBright.bold;
+const success = chalk.greenBright.bold;
+
+export default class Action {
+  static list() {
+    const tasks = DB.getAllTasks();
+    if (tasks.length) {
+      console.table(tasks);
+    } else {
+      console.log(warn("There is not any task."));
+    }
+  }
+}
